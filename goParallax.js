51323189044462c0e267e5c66b4755e7ccf817aa
teknopop-dc <48596749+teknopop-dc@users.noreply.github.com>
2022-10-14 12:22:26 +0200

    Update goParallax.js

--- goParallax.js
+++ goParallax.js
@@ -29,8 +29,8 @@ $.fn.goParallax = function(options) {
 };
 
 $(window).on('scroll', function() {
-    //$('.parallax').goParallax({orientation: "up", duration: 0.4, scale: 1.4});
-    //$('.parallax-down').goParallax({orientation: "down", duration: 0.8, scale: 1.2});
-    $('.parallax-left').goParallax({orientation: "up left", duration: 0.4, scale: 1.4});
-    $('.parallax-right').goParallax({orientation: "up right", duration: 0.4, scale: 1.4});
+    if($('.parallax')[0]) { $('.parallax').goParallax({orientation: "up", duration: 0.4, scale: 1.4}); }
+    if($('.parallax-down')[0]) { $('.parallax-down').goParallax({orientation: "down", duration: 0.8, scale: 1.2}); }
+    if($('.parallax-left')[0]) { $('.parallax-left').goParallax({orientation: "up left", duration: 0.4, scale: 1.4}); }
+    if($('.parallax-right')[0]) { $('.parallax-right').goParallax({orientation: "up right", duration: 0.4, scale: 1.4}); }
 });
